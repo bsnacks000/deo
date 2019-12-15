10d15d10c6d79d9c44312d4944a0fab29c64b537
bsnacks000 <bsnacks000@gmail.com>
2019-12-15 15:22:33 -0500

    tested byte parser

---
+++ tests/test_parsers.py
@@ -0,0 +1,29 @@
+import pytest 
+import unittest 
+tc = unittest.TestCase()
+
+from curiorpc.parsers import JSONByteParser 
+from curiorpc.exceptions import ParseError
+
+
+
+def test_json_byte_parser():
+
+    parser = JSONByteParser()
+    b = b'{"a":1,"b":[1,2,3],"c":{"hi":"bye"}}'
+    pyobj = {'a': 1, 'b': [1,2,3], 'c': {'hi': 'bye'}}
+
+    test = parser.decode(b)
+    tc.assertDictEqual(test, pyobj)
+
+    test = parser.encode(pyobj)
+    assert b == test 
+
+    with pytest.raises(ParseError):
+        parser.decode(b'{"hi"Lsdf;;,')
+    
+    class Dummy:
+        pass
+
+    with pytest.raises(ParseError):
+        parser.encode(Dummy())
